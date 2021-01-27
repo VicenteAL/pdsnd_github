913065b6dbde83f96e333033cf90d6a20289a79b
Vicente-Alemany <mvalemanyl@gmail.com>
2021-01-28 00:43:15 +0100

    Enhanced further description lines with bold and italics

--- bikeshare_2.py
+++ bikeshare_2.py
@@ -111,7 +111,7 @@ def time_stats(df):
     print('-'*40)
 
 def station_stats(df):
-    """Displays statistics on the most popular stations and trip."""
+    """Displays statistics on the most popular *stations and trip.*"""
 
     print('\nCalculating The Most Popular Stations and Trip...\n')
     start_time = time.time()
